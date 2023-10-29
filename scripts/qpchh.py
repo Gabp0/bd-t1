98393882de07dc92aea40fae998ec0be6dcef2a2
gab <gabriel.pontarolo045@gmail.com>
2023-10-29 14:18:07 -0300

    fix some calculations for qpchh

--- scripts/qpchh.py
+++ scripts/qpchh.py
@@ -81,14 +81,14 @@ def filter_throughput_test(filename:str) -> float:
 
     return total_time, geometric_mean
 
-def calculate_qpchh(query_times:np.ndarray, refresh_times:np.ndarray, total_time:float, scale_factor:int) -> float:
+def calculate_qpchh(geometric_mean:float, refresh_times:np.ndarray, total_time:float, scale_factor:int, query_streams:int) -> float:
     """
     Calculates the QPCHH metric
 
     Parameters
     ----------
-    query_times : np.ndarray
-        The completion times of each query during the power test
+    geometric_mean : float
+        The geometric mean of the query times during the throughput test
 
     refresh_times : np.ndarray
         The completion times of each refresh operation during the power test
@@ -99,17 +99,21 @@ def calculate_qpchh(query_times:np.ndarray, refresh_times:np.ndarray, total_time
     scale_factor : int
         The scale factor of the database
 
+    query_streams : int
+        The number of parallel access to the database
+
     Returns
     -------
     qpchh : float
         The QPCHH metric value
     """
 
-    prod_qi = np.prod(query_times)  # product of query times QI(i,s), TPC-H 5.3.7.2
+    # prod_qi = np.prod(query_times)  # product of query times QI(i,s), TPC-H 5.3.7.2
+    prod_qi = geometric_mean
     prod_ri = np.prod(refresh_times) # product of refresh times RI(j,s), TPC-H 5.3.7.3
 
     power_size = (3600 * scale_factor) / np.power(prod_qi * prod_ri, 1/24)  # TPC-H 5.4.1.1
-    throughput_size = (QUERY_NUMBER * 22 * 3600) / (total_time * scale_factor) # TPC-H 5.4.2.1
+    throughput_size = (query_streams * 22 * 3600) / (total_time * scale_factor) # TPC-H 5.4.2.1
 
     qpchh = np.sqrt(power_size * throughput_size) # TPC-H 5.4.3.1
 
@@ -117,10 +121,11 @@ def calculate_qpchh(query_times:np.ndarray, refresh_times:np.ndarray, total_time
 
 if __name__ == "__main__":
     if len(argv) < 3:
-        print("Usage: qpchh <power test log> <throughput test log> [scale factor]")
+        print("Usage: qpchh <power test log> <throughput test log> [scale factor] [query streams]")
         exit(1)
 
     scale_factor = int(argv[3]) if len(argv) == 4 else 1
+    query_streams = int(argv[4]) if len(argv) == 5 else 2
     
     query_times, refresh_times = filter_power_test(argv[1])
     total_time, geometric_mean = filter_throughput_test(argv[2])
@@ -131,6 +136,6 @@ if __name__ == "__main__":
     print(f"Query times: {query_times}")
     print(f"Refresh times: {refresh_times}")
 
-    qpchh = calculate_qpchh(query_times, refresh_times, total_time, scale_factor)
+    qpchh = calculate_qpchh(geometric_mean, refresh_times, total_time, scale_factor, query_streams)
 
     print(f"QPCHH: {qpchh}")
